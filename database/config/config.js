57318fcafc6f0e1b1cb63d35bce500c28f4af29d
nicolasq57 <nicolasq57@gmail.com>
2022-05-20 16:31:36 -0300

    create model activities and migration

--- database/config/config.js
+++ database/config/config.js
@@ -1,35 +1,25 @@
 require('dotenv').config()
-
 module.exports =
-
-{ 
-    development: {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": process.env.DB_DIALECT,
-
-        seederStorage: "sequelize",
-        seederStorageTableName: "seeds",
-
-        migrationStorage: "sequelize",
-        migrationStorageTableName: "migrations"
-    },
-
-    production: {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": process.env.DB_DIALECT,
-    
-        seederStorage: "sequelize",
-        seederStorageTableName: "seeds",
-    
-        migrationStorage: "sequelize",
-        migrationStorageTableName: "migrations"
-    }
-}
+{
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+};

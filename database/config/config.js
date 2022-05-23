0b8ed4656dc22c06dea783c3a5ef6501b6da2c46
nicolasq57 <nicolasq57@gmail.com>
2022-05-23 19:14:45 -0300

    fix config.js

--- database/config/config.js
+++ database/config/config.js
@@ -1,25 +1,33 @@
-require('dotenv').config()
-module.exports =
-{
+require('dotenv').config();
+
+module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'seeds',
+
+    migrationStorage: 'sequelize',
+    migrationStorageTableName: 'migrations',
   },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  },
+
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
-  }
+
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'seeds',
+
+    migrationStorage: 'sequelize',
+    migrationStorageTableName: 'migrations',
+  },
 };

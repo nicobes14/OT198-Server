fa99091154e916c47c0cecd6cc8aeff9ba4c83ff
Pablo Penalva <penalva.pablo@gmail.com>
2022-05-30 17:17:53 -0300

    isAdmin middleware added (#33)

---
+++ middlewares/isAdmin.js
@@ -0,0 +1,15 @@
+const { endpointResponse } = require('../helpers/success')
+
+module.exports = {
+  isAdmin: async (req, res, next) => {
+    if (req.user.roleId !== 1) {
+      endpointResponse({
+        res,
+        code: 401,
+        status: true,
+        message: 'Not allowed',
+      })
+    }
+    next()
+  },
+}

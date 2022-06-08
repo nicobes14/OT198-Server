015f526215d92f8f8e2a83fdd8ae641269929da8
Nicolas Bes <nicobes14@gmail.com>
2022-06-08 13:39:29 -0300

    add get new comments to news router

--- routes/news.js
+++ routes/news.js
@@ -9,10 +9,14 @@ const {
 } = require('../controllers/news')
 const { auth } = require('../middlewares/auth')
 const { isAdmin } = require('../middlewares/isAdmin')
+const { listNewsComments } = require('../controllers/comments')
 
 router.get('/:id', listNews)
 router.post('/', validateSchema(newSchema), post)
 router.put('/:id', validateSchema(newSchema), update)
 router.delete('/:id', auth, isAdmin, destroy)
+
+// get news comments
+router.get('/:id/comments', auth, listNewsComments)
 
 module.exports = router

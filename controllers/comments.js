17d8268d6741a4239c8fd83399aa0113facb81f6
Nicolas Bes <nicobes14@gmail.com>
2022-06-08 13:55:03 -0300

    add listNewsComments to comment controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,6 +1,6 @@
 const { catchAsync } = require('../helpers/catchAsync')
 const { endpointResponse } = require('../helpers/success')
-const { listComments } = require('../services/comments')
+const { listComments, listNewsCommentsService } = require('../services/comments')
 
 module.exports = {
   list: catchAsync(async (req, res) => {
@@ -11,6 +11,17 @@ module.exports = {
       status: true,
       message: 'Comments listed',
       body: comments,
+    })
+  }),
+  listNewsComments: catchAsync(async (req, res) => {
+    const { id } = req.params
+    const newsComments = await listNewsCommentsService(id)
+    endpointResponse({
+      res,
+      code: 200,
+      status: true,
+      message: `Comments of new with id ${id} listed`,
+      body: newsComments,
     })
   }),
 }

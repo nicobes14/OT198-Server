3e636b55ab385c31f47adf8dfdf35c2f891f406f
Nicolas Bes <88128116+nicobes14@users.noreply.github.com>
2022-06-09 20:55:59 -0300

    [PAGINATION] - Add pagination to members - OT198-88 (#86)
    * add pagination to member controller
    
    * add pagination to members service
    
    * add pagination util

--- controllers/members.js
+++ controllers/members.js
@@ -2,16 +2,21 @@ const httpStatus = require('../helpers/httpStatus')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 const { listMembers, createMember } = require('../services/members')
+const { calculatePagination } = require('../utils/pagination')
 
 module.exports = {
   list: catchAsync(async (req, res) => {
-    const members = await listMembers()
+    req.query.page = req.query.page || 1
+    const members = await listMembers(req.query.page)
     endpointResponse({
       res,
       code: httpStatus.OK,
       status: true,
       message: 'Members successfully retrieved',
-      body: members,
+      body: {
+        ...calculatePagination(req.query.page, members.count),
+        members: members.rows,
+      },
     })
   }),
   post: catchAsync(async (req, res) => {

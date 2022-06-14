8f0acc20726cad95c162cc5b806dd8a233380392
Pablo Penalva <penalva.pablo@gmail.com>
2022-06-14 09:50:13 -0300

    fixes (#96)

--- routes/news.js
+++ routes/news.js
@@ -5,13 +5,13 @@ const router = new express.Router()
 const { validateSchema } = require('../middlewares/validateErrors')
 const newSchema = require('../schemas/newSchema')
 const {
-  post, listNews, update, destroy,
+  post, list, update, destroy,
 } = require('../controllers/news')
 const { auth } = require('../middlewares/auth')
 const { isAdmin } = require('../middlewares/isAdmin')
 const { listNewsComments } = require('../controllers/comments')
 
-router.get('/:id', listNews)
+router.get('/:id', list)
 router.post('/', auth, isAdmin, validateSchema(newSchema), post)
 router.put('/:id', auth, isAdmin, validateSchema(newSchema), update)
 router.delete('/:id', auth, isAdmin, destroy)

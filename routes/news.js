5c9653a355277807c64200eb7c30422700e9b561
Nicolas Bes <88128116+nicobes14@users.noreply.github.com>
2022-06-14 12:06:16 -0300

    deleting not used files and fix duplicated routes (#98)

--- routes/news.js
+++ routes/news.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = new express.Router()
 
 const { validateSchema } = require('../middlewares/validateErrors')
-const newSchema = require('../schemas/newSchema')
+const newSchema = require('../schemas/new')
 const {
   post, list, update, destroy,
 } = require('../controllers/news')

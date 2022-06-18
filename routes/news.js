f426e0dc7e7aad9a8496dfe1b33ae73ffe66bd34
Noe Carosella <72532638+honeybadger2788@users.noreply.github.com>
2022-06-18 18:18:15 -0300

    add uploadImage at post and update new route, controller and service (#114)

--- routes/news.js
+++ routes/news.js
@@ -10,11 +10,12 @@ const {
 const { auth } = require('../middlewares/auth')
 const { isAdmin } = require('../middlewares/isAdmin')
 const { listNewsComments } = require('../controllers/comments')
+const { uploadImage } = require('../middlewares/uploadImage')
 
 router.get('/', list)
 router.get('/:id', listNew)
-router.post('/', auth, isAdmin, validateSchema(newSchema), post)
-router.put('/:id', auth, isAdmin, validateSchema(newSchema), update)
+router.post('/', auth, isAdmin, uploadImage('image'), validateSchema(newSchema), post)
+router.put('/:id', auth, isAdmin, uploadImage('image'), validateSchema(newSchema), update)
 router.delete('/:id', auth, isAdmin, destroy)
 
 // get news comments
